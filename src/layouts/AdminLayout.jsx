68c76d42f91d84ef282304b4ebe085a7f1f203a6
Rukshan Thathsara <136584471+rukshanthathsara@users.noreply.github.com>
2026-06-19 00:12:53 +0530

    Update AdminLayout.jsx
    updated Admin Layout

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -3,16 +3,33 @@ import { Outlet } from "react-router-dom";
 import AdminSidebar from "../components/common/AdminSidebar";
 import AdminHeader from "../components/common/AdminHeader";
 import SessionWarningModal from "../components/common/SessionWarningModal";
+import { useState } from "react";
 
 const AdminLayout = () => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   return (
     <div className="min-h-screen bg-[#F5F7FA] flex">
       <SessionWarningModal />
 
-      <AdminSidebar />
+      <AdminSidebar
+  sidebarCollapsed={sidebarCollapsed}
+  setSidebarCollapsed={setSidebarCollapsed}
+/>
 
-      <div className="flex-1 ml-[220px] flex flex-col">
-        <AdminHeader />
+      <div
+  className={`
+    flex-1
+    flex
+    flex-col
+    transition-all
+    duration-300
+    ${sidebarCollapsed ? "ml-0" : "ml-[220px]"}
+  `}
+>
+        <AdminHeader
+  sidebarCollapsed={sidebarCollapsed}
+  setSidebarCollapsed={setSidebarCollapsed}
+/>
 
         <main className="flex-1 px-6 pt-8 pb-6">
           <Outlet />
