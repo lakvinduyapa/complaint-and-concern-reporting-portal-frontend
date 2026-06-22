704196d2305706a6fc39abd229cb1ac574644aad
Rukshan Thathsara <136584471+rukshanthathsara@users.noreply.github.com>
2026-06-22 22:53:30 +0530

    Update AdminHeader.jsx
    Admin Header updated

--- src/components/common/AdminHeader.jsx
+++ src/components/common/AdminHeader.jsx
@@ -1,10 +1,8 @@
 import { useMemo } from "react"; // Added useMemo
 import { FiBell, FiMenu } from "react-icons/fi";
+import Logo from "../../assets/SLT Logo.png";
 
-const AdminHeader = ({
-  sidebarCollapsed,
-  setSidebarCollapsed,
-}) => {
+const AdminHeader = () => {
   // Memoized JSON.parse to prevent localstorage parsing on every render frame
   const currentUser = useMemo(() => {
     return JSON.parse(localStorage.getItem("adminUser") || "{}");
@@ -28,29 +26,45 @@ const AdminHeader = ({
 
   return (
     <header
-      className="h-24 mt-4 mx-4 transition-shadow hover:shadow-2xl duration-300 px-8 hidden md:flex items-center justify-between rounded-2xl shadow-l"
+  className="
+    fixed
+    top-0
+    left-0
+    right-0
+    z-50
+    h-24
+    px-8
+    hidden
+    md:flex
+    items-center
+    justify-between
+    shadow-md
+    transition-shadow
+    hover:shadow-2xl
+    duration-300
+  "
       style={{
         background: "linear-gradient(90deg, #001A72 0%, #005E5E 75%, #005E5E 100%)",
       }}
     >
       {/* Left */}
-      <div className="flex items-center gap-4">
-        <button
-          onClick={() => setSidebarCollapsed(!sidebarCollapsed)}
-          className="w-10 h-10 rounded-xl bg-white/10 border border-white/20 text-white flex items-center justify-center hover:bg-white/20 hover:scale-110 active:scale-95 transition-all duration-300"
-        >
-          <FiMenu />
-        </button>
+      <div className="flex items-center gap-5">
+  <img
+    src={Logo}
+    alt="SLTMobitel"
+    className="w-32 h-auto object-contain"
+  />
 
-        <div>
-          <h1 className="text-3xl font-bold text-white">
-            {getDashboardTitle()}
-          </h1>
-          <p className="text-slate-200 mt-1">
-            Monitor complaints and investigation activities
-          </p>
-        </div>
-      </div>
+ <div className="ml-[60px]">
+  <h1 className="text-3xl font-bold text-white">
+    {getDashboardTitle()}
+  </h1>
+
+  <p className="text-slate-200 mt-1">
+    Monitor complaints and investigation activities
+    </p>
+  </div>
+</div>
 
       {/* Right */}
       <div className="flex items-center gap-4">
